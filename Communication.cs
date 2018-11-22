514b8485b84d5a249218988fe1c75bc5324029cc
lwhgit <lwh8762@naver.com>
2018-11-23 02:38:59 +0900

    init

---
+++ Communication.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Threading;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketClient {
+    public class Communication {
+
+        public interface ICommunicationEventListener {
+            void OnConnected();
+            void OnDataReceived(byte[] buffer);
+        }
+
+        public class Client {
+            private ICommunicationEventListener eventListener = null;
+
+            private Socket socket = null;
+            private string ip = "127.0.0.1";
+            private int port = 8762;
+
+            private byte[] receiveBuffer = null;
+            private string receiveString = null;
+
+            private Queue<byte[]> queueForSend = null;
+            private Queue<byte[]> queueForReceive = null;
+
+            private Thread connectThread = null;
+            private Thread dataSendThread = null;
+            private Thread dataReceiveThread = null;
+
+            public Client() {
+                queueForSend = new Queue<byte[]>();
+                queueForReceive = new Queue<byte[]>();
+
+                connectThread = new Thread(ConnectThread);
+                connectThread.IsBackground = true;
+                dataSendThread = new Thread(DataSendThread);
+                dataSendThread.IsBackground = true;
+                dataReceiveThread = new Thread(DataReceiveThread);
+                dataReceiveThread.IsBackground = true;
+            }
+
+            public void SetCommunicationEventListener(ICommunicationEventListener listener) {
+                eventListener = listener;
+            }
+
+            public void ConnectToServer(string ip, int port) {
+                this.ip = ip;
+                this.port = port;
+                connectThread.Start();
+            }
+
+            public void Send(string data) {
+                queueForSend.Enqueue(Encoding.UTF8.GetBytes(data));
+            }
+
+            private void ConnectThread() {
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                socket.Connect(new IPEndPoint(IPAddress.Parse(ip), port));
+
+                Form1.form.Invoke(new Action(delegate () {
+                    eventListener.OnConnected();
+                }));
+
+                dataSendThread.Start();
+                dataReceiveThread.Start();
+            }
+
+            private void DataSendThread() {
+                while (true) {
+                    if (queueForSend.Count > 0) {
+                        socket.Send(queueForSend.Dequeue());
+                    }
+                    Thread.Sleep(10);
+                }
+            }
+
+            private void DataReceiveThread() {
+                while (true) {
+                    if (socket.Available > 0) {
+                        receiveBuffer = new byte[socket.Available];
+                        socket.Receive(receiveBuffer);
+                        receiveString = Encoding.UTF8.GetString(receiveBuffer);
+                        
+                        Form1.form.Invoke(new Action(delegate () {
+                            eventListener.OnDataReceived(receiveBuffer);
+                        }));
+                    }
+                    Thread.Sleep(10);
+                }
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SocketClient {
+    public partial class Form1 : Form, Communication.ICommunicationEventListener {
+        public static Form1 form = null;
+
+        private Communication.Client client = null;
+
+        public Form1() {
+            InitializeComponent();
+
+            Form1.form = this;
+            client = new Communication.Client();
+            client.SetCommunicationEventListener(this);
+        }
+
+        private void input_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter) {
+                client.Send(input.Text);
+                input.Text = "";
+            }
+        }
+
+        private void connectButton_Click(object sender, EventArgs e) {
+            client.ConnectToServer(ipInput.Text, int.Parse(portInput.Text));
+        }
+
+        public void OnConnected() {
+
+        }
+
+        public void OnDataReceived(byte[] buffer) {
+            logView.Items.Add(Encoding.UTF8.GetString(buffer));
+        }
+    }
+}
